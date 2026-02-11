dc1e30b7808ed0e9eca15dd2df5bc650de07f6a9
Oleksandr Klosovych <oklosovych@cbhbank.com>
2026-02-11 15:55:14 +0100

    Initial Commit

---
+++ Subscription Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Subscription Tracker
+//
+//  Created by Oleksandr Klosovych on 11.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Subscription Tracker/Subscription_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Subscription_TrackerApp.swift
+//  Subscription Tracker
+//
+//  Created by Oleksandr Klosovych on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Subscription_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
